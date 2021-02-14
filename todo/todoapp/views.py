ecfda72f6c3f016c88bda0bb4382aeee205dce33
manojkale123 <manojvkale1994@gmail.com>
2021-02-14 09:33:08 +0200

    my first commit

---
+++ todo/todoapp/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render,redirect
+from .models import todo
+
+# Create your views here.
+def home(request):
+    data =todo.objects.all()
+    return render(request,'home.html',{'data':data})
+
+def add(request):
+    tododata = request.POST['todo']
+    todo_item = todo(content=tododata)
+    todo_item.save()
+    return redirect(home)
+
+def deleteItem(request,todo_id):
+    item = todo.objects.get(id=todo_id)
+    item.delete()
+    return redirect(home)
